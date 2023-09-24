5237964865140ab17b68ca88188524b17547a08d
stormymayday <stormymayday7@gmail.com>
2023-09-23 18:09:44 -0700

    Importing API in main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import Store from './services/Store.js';
+import API from './services/API.js';
 // import icon from './src/icons/icon.png';
 // import logoSVG from './src/logo.svg';
 import logoPNG from './src/logo.png';
@@ -9,6 +10,7 @@ import menu from './src/menu.json';
 window.addEventListener("DOMContentLoaded", () => {
 
   console.log(Store);
+  console.log(API.fetchMenu());
 
 });
 
