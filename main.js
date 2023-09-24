37ac4c681cd219f4069fea761ce4221e7164cc3c
stormymayday <stormymayday7@gmail.com>
2023-09-23 18:23:21 -0700

    Creaated the Menu.js file that calls the API and stores response in the window.app.store

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import './style.css';
 import Store from './services/Store.js';
 import API from './services/API.js';
-// import icon from './src/icons/icon.png';
-// import logoSVG from './src/logo.svg';
 import logoPNG from './src/logo.png';
 import blackamericano from './src/images/blackamericano.png';
 
@@ -13,10 +11,7 @@ window.app = {};
 app.store = Store;
 
 
-window.addEventListener("DOMContentLoaded", () => {
-
-  console.log(window.app);
-  console.log(API.fetchMenu());
+window.addEventListener("DOMContentLoaded", async () => {
 
 });
 
---
+++ services/Menu.js
@@ -0,0 +1,7 @@
+import API from "./API.js";
+
+export async function loadData() {
+
+    app.store.menu = await API.fetchMenu();
+
+}
