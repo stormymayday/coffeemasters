a69b84f45aca9d10b30e428cc6b38e656dca6b89
stormymayday <stormymayday7@gmail.com>
2023-09-23 18:17:44 -0700

    Made Store global by attaching it to the window

--- main.js
+++ main.js
@@ -5,11 +5,17 @@ import API from './services/API.js';
 // import logoSVG from './src/logo.svg';
 import logoPNG from './src/logo.png';
 import blackamericano from './src/images/blackamericano.png';
-import menu from './src/menu.json';
+
+// Making Store global:
+// 1. Attaching app object to the window
+window.app = {};
+// 2. Hooking Store to the app.store
+app.store = Store;
+
 
 window.addEventListener("DOMContentLoaded", () => {
 
-  console.log(Store);
+  console.log(window.app);
   console.log(API.fetchMenu());
 
 });
