afb6745e360182d3c0521306a9bea00ed80d623b
stormymayday <stormymayday7@gmail.com>
2023-09-23 18:07:48 -0700

    Importing Store in main.js

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 import './style.css';
+import Store from './services/Store.js';
 // import icon from './src/icons/icon.png';
 // import logoSVG from './src/logo.svg';
 import logoPNG from './src/logo.png';
 import blackamericano from './src/images/blackamericano.png';
 import menu from './src/menu.json';
 
-// Helpful Shorthand Methods
-// const $ = () => document.querySelector.call(this, arguments);
-// const $$ = () => document.querySelectorAll.call(this, arguments);
-// HTMLElement.prototype.on = (a, b, c) => this.addEventListener(a, b, c);
-// HTMLElement.prototype.off = (a, b) => this.removeEventListener(a, b);
-// HTMLElement.prototype.$ = (s) => this.querySelector(s);
-// HTMLElement.prototype.$ = (s) => this.querySelectorAll(s);
-
 window.addEventListener("DOMContentLoaded", () => {
 
-  let nav = document.querySelector("nav");
-  console.log(nav);
-
-  nav.innerHTML = `
-    <h2>Hello DOM</h2>
-    <p>This HTML is from JavaScript</p>
-  `;
-
-  document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello World</h1>
-  </div>
-`;
+  console.log(Store);
 
 });
 
-window.addEventListener("DOMContentLoaded", event => {
-
-});
-
-console.log(menu);
