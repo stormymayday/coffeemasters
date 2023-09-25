8e8e52c5f2c0ee782d31a1988d34c07538a056e9
stormymayday <stormymayday7@gmail.com>
2023-09-25 11:18:23 -0700

    Setting a trap for set method in the proxyStore

--- services/Store.js
+++ services/Store.js
@@ -6,6 +6,37 @@ const Store = {
 // Creating a Proxy
 const proxyStore = new Proxy(Store, {
 
+    // Trap for set
+    set(targetObject, propertyName, propertyValue) {
+
+        // Assigning the value
+        targetObject[propertyName] = propertyValue;
+
+        // Checking if property is 'menu'
+        if (propertyName === 'menu') {
+
+            // Announcing that the menu was changed
+            // Note: using window instead of document
+            // Because: we have two documents
+            // window is a global document
+            // Shadow DOM is a local document
+            window.dispatchEvent(new Event('app-menu-change'));
+
+        }
+
+        if (propertyName === 'cart') {
+
+            window.dispatchEvent(new Event('app-cart-change'));
+
+        }
+
+        // Important:
+        // Must return 'true' if we are accepting the set
+        // Otherwise, must return 'false'
+        return true;
+
+    }
+
 });
 
 export default proxyStore;
