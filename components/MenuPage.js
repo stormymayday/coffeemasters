8bd314d3d2017761e52351754b2848521852b725
stormymayday <stormymayday7@gmail.com>
2023-09-24 21:59:52 -0700

    Loading styles for the MenuPage.js

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -4,22 +4,43 @@ export default class MenuPage extends HTMLElement {
 
         super();
 
+        // Creating Shadow DOM with mode 'open'
+        this.root = this.attachShadow({ mode: 'open' });
+
+        // Creating styles object
+        const styles = document.createElement('style');
+
+        // Attaching styles to Shadow DOM
+        this.root.appendChild(styles);
+
+        // Loading the  CSS
+        async function loadCSS() {
+
+            // Fetching the CSS
+            const request = await fetch("./components/MenuPage.css");
+
+            // 
+            const css = await request.text();
+
+            // Filling the styles object
+            styles.textContent = css;
+
+        }
+        loadCSS();
+
     }
 
     // When component is attached to the DOM
     connectedCallback() {
 
         // Getting the template from the DOM
-        // At this point template is not usable
-        // It must be clone first
         const template = document.getElementById('menu-page-template');
 
         // Cloning the template
-        // true = deep clone (template might contain other templates / elements inside)
         const content = template.content.cloneNode(true);
 
-        // 
-        this.appendChild(content);
+        // Appending the content
+        this.root.appendChild(content);
 
     }
 
