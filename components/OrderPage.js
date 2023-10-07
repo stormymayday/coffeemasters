02bf514752c0187d2a86bf8d1000d8567491bc51
stormymayday <stormymayday7@gmail.com>
2023-10-06 21:41:34 -0700

    Displaying Items in the Cart

--- components/OrderPage.js
+++ components/OrderPage.js
@@ -133,7 +133,7 @@ export default class OrderPage extends HTMLElement {
 
         `;
 
-        const section = document.createComment('section');
+        const section = document.createElement('section');
 
         this.root.appendChild(section);
 
@@ -155,8 +155,47 @@ export default class OrderPage extends HTMLElement {
 
     render() {
 
+        let section = this.root.querySelector("section");
+
+        if (app.store.cart.length == 0) {
+
+            section.innerHTML = `
+                <p class="empty">Your order is empty</p>
+            `;
+
+        } else {
+            let html = `
+                <h2>Your Order</h2>
+                <ul>
+                </ul>
+            `;
+
+            section.innerHTML = html;
+
+            const template = document.getElementById("order-form-template");
+
+            const content = template.content.cloneNode(true);
+            section.appendChild(content);
+
+            let total = 0;
+
+            for (let prodInCart of app.store.cart) {
+                const item = document.createElement("cart-item");
+                item.dataset.item = JSON.stringify(prodInCart);
+                this.root.querySelector("ul").appendChild(item);
+
+                total += prodInCart.quantity * prodInCart.product.price;
+            }
+
+            this.root.querySelector("ul").innerHTML += `
+            <li>
+                <p class='total'>Total</p>
+                <p class='price-total'>$${total.toFixed(2)}</p>
+            </li>                
+        `;
 
 
+        }
     }
 }
 
